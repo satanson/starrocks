15e99fcab00c7c8c28b50d4a8dd3358df76e14ad
yiming <107105845+nshangyiming@users.noreply.github.com>
2023-08-03 10:03:49 +0800

    [BugFix] PUBLIC role should not in immutable names set (#28508)
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>
    (cherry picked from commit ad7172c02ee30371472192156f556a3cd5b7366e)

--- fe/fe-core/src/main/java/com/starrocks/privilege/AuthorizationMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/privilege/AuthorizationMgr.java
@@ -1275,7 +1275,7 @@ public class AuthorizationMgr {
     }
 
     public boolean isBuiltinRole(String name) {
-        return PrivilegeBuiltinConstants.IMMUTABLE_BUILT_IN_ROLE_NAMES.contains(name);
+        return PrivilegeBuiltinConstants.BUILT_IN_ROLE_NAMES.contains(name);
     }
 
     public String getRoleComment(String name) {
