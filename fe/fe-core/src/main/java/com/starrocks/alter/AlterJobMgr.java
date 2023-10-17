02cea471a8f142b53c6a5e606c44dcca250aa0f2
Murphy <mofei@starrocks.com>
2023-10-17 12:55:28 +0800

    fix conflict
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/AlterJobMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/AlterJobMgr.java
@@ -300,7 +300,6 @@ public class AlterJobMgr {
         }
     }
 
-<<<<<<< HEAD
     public void processAlterMaterializedView(AlterMaterializedViewStmt stmt)
             throws DdlException, MetaNotFoundException {
         // check db
@@ -405,10 +404,6 @@ public class AlterJobMgr {
 
         if (AlterMaterializedViewStmt.ACTIVE.equalsIgnoreCase(status)) {
             String viewDefineSql = materializedView.getViewDefineSql();
-=======
-    public void alterMaterializedViewStatus(MaterializedView materializedView, String status, boolean isReplay) {
-        if (AlterMaterializedViewStatusClause.ACTIVE.equalsIgnoreCase(status)) {
->>>>>>> d80283657f ([BugFix] check the schema when trying to activate the mv (#32698))
             ConnectContext context = new ConnectContext();
             context.setGlobalStateMgr(GlobalStateMgr.getCurrentState());
             context.setQualifiedUser(AuthenticationMgr.ROOT_USER);
@@ -426,12 +421,8 @@ public class AlterJobMgr {
             }
 
             // Skip checks to maintain eventual consistency when replay
-<<<<<<< HEAD
-            List<BaseTableInfo> baseTableInfos = MaterializedViewAnalyzer.getBaseTableInfos(queryStatement, !isReplay);
-=======
             List<BaseTableInfo> baseTableInfos =
                     Lists.newArrayList(MaterializedViewAnalyzer.getBaseTableInfos(mvQueryStatement, !isReplay));
->>>>>>> d80283657f ([BugFix] check the schema when trying to activate the mv (#32698))
             materializedView.setBaseTableInfos(baseTableInfos);
             materializedView.getRefreshScheme().getAsyncRefreshContext().clearVisibleVersionMap();
             GlobalStateMgr.getCurrentState().updateBaseTableRelatedMv(materializedView.getDbId(),
