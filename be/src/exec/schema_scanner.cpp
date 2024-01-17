eb9b46520109c1f4ed8d8cea80e70d495379d972
andyziye <108652123+andyziye@users.noreply.github.com>
2024-01-17 22:19:17 +0800

    [Feature] sys.fe_locks (backport #35080)(sync #39191) (#12334)
    Signed-off-by: wyb <wybb86@gmail.com>
    Co-authored-by: wyb <wybb86@gmail.com>

--- be/src/exec/schema_scanner.cpp
+++ be/src/exec/schema_scanner.cpp
@@ -46,6 +46,7 @@
 #include "exec/schema_scanner/starrocks_grants_to_scanner.h"
 #include "exec/schema_scanner/starrocks_policy_references_scanner.h"
 #include "exec/schema_scanner/starrocks_role_edges_scanner.h"
+#include "exec/schema_scanner/sys_fe_locks.h"
 #include "exec/schema_scanner/sys_object_dependencies.h"
 
 namespace starrocks {
@@ -167,6 +168,8 @@ std::unique_ptr<SchemaScanner> SchemaScanner::create(TSchemaTableType::type type
         return std::make_unique<StarrocksPolicyReferencesScanner>();
     case TSchemaTableType::STARROCKS_OBJECT_DEPENDENCIES:
         return std::make_unique<SysObjectDependencies>();
+    case TSchemaTableType::SYS_FE_LOCKS:
+        return std::make_unique<SysFeLocks>();
     default:
         return std::make_unique<SchemaDummyScanner>();
     }
